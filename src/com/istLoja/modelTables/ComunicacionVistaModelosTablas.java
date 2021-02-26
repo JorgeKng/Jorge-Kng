177d3c301a3ebffd965824623ad2414de9456e71
Jorge Kng <Jorge Kng@DESKTOP-00736DG>
2021-02-26 10:40:44 -0500

    Konektion BD

---
+++ src/com/istLoja/modelTables/ComunicacionVistaModelosTablas.java
@@ -0,0 +1,14 @@
+package com.istloja.modelTables;
+
+import com.istloja.modelo.Persona;
+import com.istloja.modelo.Proveedores;
+
+/**
+ *
+ * @author johnp
+ */
+public interface ComunicacionVistaModelosTablas {
+   void clickPersona(Persona p);
+   void clickProveedores(Proveedores p);
+   
+}
